20537a63280ee0acd300df903884537ce7ae5306
Freshy4K <sammiroberhansley@gmail.com>
2024-12-07 18:15:54 -0700

    car2done

---
+++ car.py
@@ -0,0 +1,57 @@
+# Problem 1
+print("Sammir Oberhansley")
+
+# Problem 2: Create an array named 'cars'
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+
+# Problem 3: Print the array to the console
+print(cars)
+
+# Problem 4: Print the length of the array to the console
+print(len(cars))
+
+# Problem 5: Append 'Buick' to the array
+cars.append('Buick')
+
+# Problem 6 Print the array to the console
+
+print(cars)
+
+# Problem 7 Print the 4th element in the array (element 4, not index 4)
+
+print(cars[3])
+
+# Problem 8: Insert 'Toyota' into element 3 in the array
+
+cars.insert(2, 'Toyota')
+
+# Problem 9: Print the array to the console
+
+print(cars)
+
+# Problem 10: Remove element 5 of the array
+cars.pop(4)
+
+# Problem 11: Print the array to the console
+print(cars)
+
+# Problem 12: Sort the array in ascending order
+cars.sort()
+
+# Problem 13: Print the array to the console
+print(cars)
+
+# Problem 14: Sort the array in descending order
+cars.sort(reverse=True)
+
+# Problem 15: Print the array to the console
+print(cars)
+
+# Problem 16: Create a variable called my_array_length with the length of the cars array
+my_array_length = len(cars)
+
+# Problem 17: Create a variable called array_string
+array_string = 'The length of my array is '
+
+# Problem 18: Print array_string concatenated with my_array_length to the console
+print(array_string + str(my_array_length))
