f46045f9d27cefeac4d6f4df594d6f05099cdbb9
Freshy4K <sammiroberhansley@gmail.com>
2024-12-07 18:20:30 -0700

    car2doneagain

--- car.py
+++ car.py
@@ -14,27 +14,21 @@ print(len(cars))
 cars.append('Buick')
 
 # Problem 6 Print the array to the console
-
 print(cars)
 
 # Problem 7 Print the 4th element in the array (element 4, not index 4)
-
 print(cars[3])
 
 # Problem 8: Insert 'Toyota' into element 3 in the array
-
 cars.insert(2, 'Toyota')
 
 # Problem 9: Print the array to the console
-
 print(cars)
 
 # Problem 10: Remove element 5 of the array
 cars.pop(4)
-
 # Problem 11: Print the array to the console
 print(cars)
-
 # Problem 12: Sort the array in ascending order
 cars.sort()
 
@@ -47,11 +41,8 @@ cars.sort(reverse=True)
 # Problem 15: Print the array to the console
 print(cars)
 
-# Problem 16: Create a variable called my_array_length with the length of the cars array
 my_array_length = len(cars)
 
-# Problem 17: Create a variable called array_string
 array_string = 'The length of my array is '
 
-# Problem 18: Print array_string concatenated with my_array_length to the console
 print(array_string + str(my_array_length))
